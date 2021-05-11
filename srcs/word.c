5f78758efd17f5ebc9052bd4b6234b14cf04b9a5
AtheDev <athe.dev@gmail.com>
2021-05-11 15:18:47 +0200

    Fix suppression des espaces dans les var d'env

--- srcs/word.c
+++ srcs/word.c
@@ -6,7 +6,7 @@
 /*   By: adupuy <adupuy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 11:17:19 by adupuy            #+#    #+#             */
-/*   Updated: 2021/05/08 16:41:57 by adupuy           ###   ########.fr       */
+/*   Updated: 2021/05/11 15:17:44 by adupuy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,7 +76,9 @@ int	delete_all_space2(char **cmd, int *i)
 			*cmd = delete_char(*cmd, i);
 			if (*cmd == NULL)
 				return (error_msg(2, ' '));
+			(*i)++;
 		}
+		(*i)--;
 	}
 	else
 	{
@@ -94,22 +96,7 @@ int	delete_all_space(char **cmd, int i)
 {
 	while ((*cmd)[++i] != '\0')
 	{
-		if ((*cmd)[i] != '"' && (*cmd)[i] != ' ' && (*cmd)[i] != '\'')
-			delete_space2(cmd, &i, 0);
-		else if (db_quotes(*cmd, i) == 0)
-		{
-			i++;
-			while (((*cmd)[i] != '"' || ((*cmd)[i] == '"' &&
-			is_escaped(*cmd, i - 1) == 1)))
-				i++;
-		}
-		else if (sp_quote(*cmd, i) == 0)
-		{
-			i++;
-			while ((*cmd)[i] != '\'')
-				i++;
-		}
-		else if ((*cmd)[i] == ' ' && is_escaped(*cmd, i - 1) == 0)
+		if ((*cmd)[i] == ' ')
 		{
 			if (delete_all_space2(cmd, &i) == -1)
 				return (-1);
